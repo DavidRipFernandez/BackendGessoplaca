c6be0f324863b8851c4ed771541ae49c50926c06
DavidRipalda <jdripalda@nivelnet.com>
2025-01-19 18:48:08 -0400

    Add project files.

---
+++ NSoft/Program.cs
@@ -0,0 +1,42 @@
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Detectar el entorno actual (Development, Production, etc.)
+var environment = builder.Environment.EnvironmentName;
+
+Console.WriteLine($"Ejecutando en entorno: {environment}");
+
+// Mostrar todas las variables de entorno dentro del contenedor
+foreach (var env in Environment.GetEnvironmentVariables().Keys)
+{
+    Console.WriteLine($"{env} = {Environment.GetEnvironmentVariable(env.ToString())}");
+}
+
+
+//configurar el archivo de configuracion
+builder.Services.AddControllers();
+
+//configura el archivo de configuración según el entorno
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true) //archivo base
+    .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange:true) //sobreescribe con el del entorno 
+    .AddEnvironmentVariables(); //permite variables de entorno de docker
+
+var app = builder.Build();
+
+//Mostrando el entorno actual en consola
+Console.WriteLine($"Ejecutando en entorno : {environment}");
+
+if (environment == "Development")
+{
+    //configuraciones adicionales para desarrollo
+    app.UseDeveloperExceptionPage();
+}
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
